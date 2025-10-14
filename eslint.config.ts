b1aab807417a2747979554d4b8684afa398dae39
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-15 07:56:48 +0800

    refactor: update the eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,23 +1,18 @@
-import {
-  combine,
-  ignores,
-  imports,
-  javascript,
-  react,
-  typescript,
-} from "@antfu/eslint-config"
+import config from "@antfu/eslint-config"
 
-export default combine(
-  ignores(["build/**", ".react-router/**"]),
-  javascript(),
-  imports(),
-  typescript(),
-  react({
+export default config({
+  stylistic: false,
+  typescript: {
+    overrides: {
+      "node/prefer-global/process": "off",
+    },
+  },
+  react: {
     overrides: {
       "react-dom/no-dangerously-set-innerhtml": "off",
       "react-hooks-extra/no-direct-set-state-in-use-effect": "off",
       "react-refresh/only-export-components": "off",
       "react/no-unstable-context-value": "off",
     },
-  }),
-)
+  },
+})
